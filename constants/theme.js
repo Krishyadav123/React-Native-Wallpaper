1c536bab3c9e4db6ccdd07c94e42f867e1f8323c
krishyadav123 <krishyadav28494@gmail.com>
2024-12-23 13:05:38 +0530

    initial commit

---
+++ constants/theme.js
@@ -0,0 +1,22 @@
+export const theme = {
+    colors: {
+        white: '#fff',
+        black: '#000',
+        grayBG: '#e5e5e5',
+        neutal: (opacity) => `rgba(10, 10, 10, ${opacity})`,
+    },
+    fontWeights: {
+        regular: '400',
+        medium: '500',
+        semiBold: '600',
+        bold: '700',
+    },
+    fontSizes: {
+        xs: 10,
+        sm: 12,
+        md: 14,
+        lg: 16,
+        xl: 18,
+        xxl: 20,
+    },
+}
